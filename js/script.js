5de6492e6b69af7ac614a47e4bf31dfe108ba987
Rustam <punk60826@gmail.com>
2020-10-03 05:00:46 +0300

    filter products

--- js/script.js
+++ js/script.js
@@ -55,3 +55,34 @@ $(document).ready(function () {
   });
 
 });
+
+//filter products
+
+(function(){
+  const elm = document.querySelector(".offers");
+  const iso = new Isotope(elm, {
+      itemSelector: ".offers__element",
+      filter: '.popular'
+  });
+  
+  const controlls = document.querySelectorAll(".filter__link");
+  const activeClass = "filter__item--active";
+  
+  controlls.forEach(function(control){
+      control.addEventListener("click", function(e){
+          e.preventDefault();
+  
+          const filterName = control.getAttribute("data-filter");
+  
+          controlls.forEach(function(link){
+              link.closest(".filter__item").classList.remove(activeClass)
+          });
+          control.closest(".filter__item").classList.add(activeClass);
+  
+          iso.arrange({
+              filter: `.${filterName}`
+          })
+      });
+  });
+  })();
+  
